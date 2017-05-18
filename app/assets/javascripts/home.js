ea1f0a9e4d2e9cfd8905f308da2b39f4c04ef7b0
Jeanette Rivera <jeanetterivera465@gmail.com>
2017-05-17 21:59:07 -0400

    added carousel to home, started form styling

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,19 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    // carousel js
+    var slides = document.querySelectorAll('#slides .slide');
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide,4000);
+
+    function nextSlide() {
+        slides[currentSlide].className = 'slide';
+        currentSlide = (currentSlide+1)%slides.length;
+        slides[currentSlide].className = 'slide showing';
+    }
+
+})
